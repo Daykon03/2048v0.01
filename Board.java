2d1948d4c093cad6d617154bbc6e2f97942b076f
Daykon03 <32910080+Daykon03@users.noreply.github.com>
2017-10-23 13:40:46 +0200

    Add files via upload

--- Board.java
+++ Board.java
@@ -3,48 +3,126 @@ package tp.pr1;
 public class Board {
 	private Cell board[][];
 	private int dim;
+	private static final String NEWLINE = System.getProperty("line.separator");
 	
 	public Board(int dim)
 	{
 		this.dim=dim;
-		this.board[dim][dim] = new Cell(); //I seriously doubt this
+		this.board = new Cell[dim][dim];
+		int k = 0;
+		for (int i = 0; i < dim; i++)
+			for (int j = 0; j < dim; j++)
+			{
+				this.board[i][j] = new Cell(k);
+				k++;
+			}
+				
 	}
 	
-	public void toString()
+	
+	public String toString()
 	{
+		String board = " ";
+		board += MyStringUtils.repeat("-" ,8 * dim);
+		board += NEWLINE;
+		for (int i = 0; i < this.dim; i++)
+		{
+			for (int j = 0; j < this.dim; j++)
+			{
+				board += "|";
+				if (this.board[i][j].getValue() != 0)
+					board += MyStringUtils.centre(this.board[i][j].toString(), 7);
+				else 
+					board += "       ";
+			}
+			board += "|";
+			board += NEWLINE;
+			
+		}
+		board += " ";
+		board += MyStringUtils.repeat("-",8 * dim);
+		board += NEWLINE;
 		
+		return board;
 	}
+
 	
 	public void setCells(Position pos, int value)
 	{
 		board[pos.getx()][pos.gety()].setValue(value);
 	}
 	
-	public MoveResults executeMove(Direction dir)
+	private void transform(Direction dir)
 	{
-		Board transformedBoard = new Board(this.dim);
-		MoveResults results = new MoveResults();
 		switch (dir)
 		{
-		case RIGHT:
-			break;
 		case LEFT: 
-			transformedBoard.invert();
+			this.invert();
 			break;
 		case UP:
-			transformedBoard.transpose();
+			this.transpose();
 			break;
 		case DOWN:
-			transformedBoard.transpose();
-			transformedBoard.invert();
+			this.transpose();
+			this.invert();
+		default:
+			break;
 			
 		}
-		results = MoveRowRight();
 	}
 	
-	private MoveResults MoveRowRight()
+	private void transformBack(Direction dir)
 	{
-
+		if (dir == Direction.DOWN)
+		{
+			this.invert();
+			this.transpose();
+		}
+		else transform(dir);
+	}
+	
+	public MoveResults executeMove(Direction dir)
+	{
+		MoveResults results = new MoveResults();
+		transform(dir);
+		for (int i = 0; i < this.dim; i++)
+			results = this.moveRowRight(i);
+		transformBack(dir);
+		return results;
+	}
+	
+	private MoveResults moveRowRight(int row)
+	{
+		MoveResults results = new MoveResults();
+		boolean end = false;
+		int i = dim - 1, j;
+		
+		while (i >= 0 && !end)
+		{
+			j = i - 1;
+			while (j >= 0 && board[row][j].getValue() == 0 )
+				j--;
+			end = j == -1;
+			if (!end)
+			{
+				if (board[row][i].getValue() == 0)
+				{
+					board[row][i].setValue(board[row][j].getValue());
+					board[row][j].setValue(0);
+					results.setMoved(true);
+				}
+				else
+					if (board[row][i].doMerge(board[row][j]))
+					{
+						results.setMoved(true);
+						results.setScore(board[row][i].getValue());
+						results.setMax(board[row][i].getValue());
+					}
+			}
+			i--;
+		}
+		
+		return results;
 	}
 	
 	private Board invert()
@@ -56,7 +134,7 @@ public class Board {
 				newBoard.board[dim - 1 - i][j] = this.board[i][j];
 			}
 		return newBoard;
-	}//Sin probar
+	}//Sin probar //Hacer con ArrayAsList
 	
 	private Board transpose()
 	{
@@ -67,5 +145,13 @@ public class Board {
 				newBoard.board[j][i] = this.board[i][j];
 			}
 		return newBoard;
-	}//Tampoco
+	}//Tampoco //Hacer con ArrayAsList
+	
+	public static void main(String args[])
+	{
+		Board board = new Board(3);
+		System.out.print(board);
+		board = board.transpose();
+		System.out.print(board);
+	}
 }
--- Cell.java
+++ Cell.java
@@ -7,14 +7,26 @@ public class Cell {
 		value = 0;
 	}
 	
+	public Cell(int n){
+		value = n;
+	}
+	
 	public int getValue(){
 		return this.value;
 	}
 	public void setValue(int value){
 		this.value=value;
 	}
-	public boolean Domerge(Cell cell){
-		return cell.value==this.value;
+	public boolean doMerge(Cell cell){
+		if (cell.value==this.value)
+		{	
+			this.value *= 2;
+			cell.value = 0;
+			return true;
+		}
+		return false;
 	}
-	
+	public String toString(){
+		return Integer.toString(this.value); //?
+	}
 }
